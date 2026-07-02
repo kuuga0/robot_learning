3cd701e79c71957b1642e2af8433cf3167a7f236
wyp <3427571575@qq.com>
2026-07-02 11:31:50 +0800

    more test

--- test_for_rd.py
+++ test_for_rd.py
@@ -1,27 +1,31 @@
 import mujoco
 import mujoco.viewer
 import time
+import glfw
 from robot_descriptions import panda_mj_description
 from robot_descriptions.loaders.mujoco import load_robot_description
 
 model =mujoco.MjModel.from_xml_path(panda_mj_description.MJCF_PATH)
 # model = load_robot_description("panda_mj_description")
-# print(panda_mj_description.MJCF_PATH)
+print(panda_mj_description.MJCF_PATH)
 data = mujoco.MjData(model)
-mujoco.mj_saveLastXML("mjmodel.xml", model)
 
-
-mujoco.viewer.launch(model, data)
-print("view is closed")
-# ctrl=data.ctrl
-# with mujoco.viewer.launch_passive(model, data) as viewer:
-#     s=time.time()
-#     while viewer.is_running() and time.time()-s<10:
-#         step_time = time.time()
-#         mujoco.mj_step(model, data)
-#         with viewer.lock():
-#             viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = (int)(data.time%2)
-#         viewer.sync()
-#         next_time = model.opt.timestep-(time.time()-step_time)
-#         if next_time>0:
-#             time.sleep(next_time)    
+# mujoco.viewer.launch(model, data)
+# print("view is closed")
+try:
+    with mujoco.viewer.launch_passive(model, data) as viewer:
+        s=time.time()
+        while viewer.is_running() and time.time()-s<30:
+            step_time = time.time()
+            mujoco.mj_step(model, data)
+            with viewer.lock():
+                viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = (int)(data.time%2)
+            viewer.sync()
+            next_time = model.opt.timestep-(time.time()-step_time)
+            if next_time>0:
+                time.sleep(next_time)  
+        print("simulation is done")  
+        while viewer.is_running():
+            time.sleep(0.01)
+except Exception as e:
+    print(f"Error occurred: {e}")
